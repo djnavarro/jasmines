586972518866f3715c4c571c70a0260f98611588
Danielle Navarro <d.navarro@unsw.edu.au>
2019-10-05 19:24:20 +1000

    add seed generating functions

--- R/tempest.R
+++ R/tempest.R
@@ -1,29 +1,79 @@
 
+#' Generate a random set of sticks
+#'
+#' @param n how many sticks
+#' @param grain how many points along each stick
+#'
+#' @return a tibble with columns x, y and id
+#' @export
+make_sticks <- function(n = 10, grain = 1000) {
+  make_stick <- function(id, grain) {
+    return(tibble::tibble(
+      x = seq(stats::runif(1), stats::runif(1), length.out = grain),
+      y = seq(stats::runif(1), stats::runif(1), length.out = grain),
+      id = id
+    ))
+  }
+  points <- purrr::map_dfr(1:n, make_stick, grain = grain)
+  return(points)
+}
+
+#' Generate a random set of bubbles
+#'
+#' @param n how many bubbles
+#' @param grain how many points along each stick
+#'
+#' @return a tibble with columns x, y and id
+#' @export
+make_bubbles <- function(n = 2, grain = 1000) {
+
+  make_bubble <- function(id, grain) {
+
+    radius <- stats::runif(1)
+    origin_x <- stats::runif(1)
+    origin_y <- stats::runif(1)
+    th <- seq(0, 2*pi, length.out = grain)
+
+    return(tibble::tibble(
+      x = radius * cos(th) + origin_x,
+      y = radius * sin(th) + origin_y,
+      id = id
+    ))
+  }
+
+  points <- purrr::map_dfr(1:n, make_bubble, grain = grain)
+  return(points)
+}
+
+
+
 
 
 #' Generate a tempest image
 #'
 #' @param file where to save the file
-#' @param seeds how many "sticks" to use as the seed?
+#' @param seed data frame with x, y, id
 #' @param iterations how many times should we iterate the curl noise?
 #' @param scale how large is each curl step?
 #' @param alpha how transparent is each line?
-#' @param grain how many points comprise a stick?
 #' @param width how wide are the lines?
+#' @param box vector specifying the edges
+#' @param zoom rescale image to fit in the unit square?
 #' @param palette function generating a palette (default: viridis)
 #' @param ... arguments to be passed to palette
 #'
 #' @export
 tempest <- function(
   file,
-  seeds = 6,      # how many seed lines?
+  seed = make_sticks(), # seed points
   iterations = 6, # how many iterations to curl?
-  scale = .02,    # size of the curl step
-  alpha = .3,     # transparency of each line
-  grain = 10000,  # number of points in each stick
-  width = 6,      # width of each line
+  scale = .02, # size of the curl step
+  alpha = .3, # transparency of each line
+  width = 6, # width of each line
+  box = NULL, # size of the box
+  zoom = TRUE, # zoom in/out so that the final image fits in unit square
   palette = NULL, # function to generate palette (args: n, alpha, ...)
-  ...             # options to pass to palette function
+  ... # options to pass to palette function
 ) {
 
   # default palette
@@ -33,24 +83,12 @@ tempest <- function(
     }
   }
 
-  # a "stick" is just a random line
-  make_stick <- function(id, n = grain) {
-    tibble::tibble(
-      x = seq(stats::runif(1), stats::runif(1), length.out = n),
-      y = seq(stats::runif(1), stats::runif(1), length.out = n),
-      id = id
-    )
-  }
-
-  # create a set of sticks to use as the seed
-  sticks <- purrr::map_dfr(1:seeds, make_stick)
-
   # create a colour palette
-  cols <- palette(nrow(sticks), alpha)
+  cols <- palette(nrow(seed), alpha, ...)
 
   # iterate each point through curl noise
   ribbon <- list()
-  ribbon[[1]] <- sticks
+  ribbon[[1]] <- seed
   ribbon[[1]]$z <- 0
 
   for(i in 1:iterations) {
@@ -67,8 +105,14 @@ tempest <- function(
     ribbon[[i+1]]$z <- order(ribbon[[i+1]]$z)
 
     # rescale the curl noise
-    ribbon[[i+1]]$x <- ribbon[[i]]$x + ribbon[[i+1]]$x * scale
-    ribbon[[i+1]]$y <- ribbon[[i]]$y + ribbon[[i+1]]$y * scale
+    if(is.null(box)) {
+      ribbon[[i+1]]$x <- ribbon[[i]]$x + ribbon[[i+1]]$x * scale
+      ribbon[[i+1]]$y <- ribbon[[i]]$y + ribbon[[i+1]]$y * scale
+    }
+  }
+
+  if(is.null(box)) {
+    box <- c(xmin = -.05, xmax = 1.05, ymin = -.05, ymax = 1.05)
   }
 
   # min, max
@@ -81,7 +125,7 @@ tempest <- function(
   gap <- .05
   op <- graphics::par(bg = "black", pty = "s")
   graphics::plot.new()
-  graphics::plot.window(c(-gap, 1 + gap), c(-gap, 1 + gap))
+  graphics::plot.window(xlim = box[1:2], ylim = box[3:4])
 
   # plot a series of curl iterations
   for(i in 1:iterations) {
