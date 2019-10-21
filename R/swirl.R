76b6aa026b7f87c88210b232b4e431bb1b81a1b1
Danielle Navarro <d.navarro@unsw.edu.au>
2019-10-21 13:45:07 +1100

    swirl separated into coil and style_wrap

--- R/swirl.R
+++ R/swirl.R
@@ -1,14 +1,12 @@
 
-#' Swirl
+#' Coil
 #'
 #' @param bridge bridge tibble
 #' @param scale length of swirl
-#' @param type plot type (line, point, barbell)
 #' @export
-swirl <- function(
-  bridge = make_bridges(50, 50),
-  scale = .02,
-  type = "line"
+coil <- function(
+  bridge = meander(seed = 50, length = 50),
+  scale = .02
 ) {
 
   bridge <- bridge %>%
@@ -23,17 +21,40 @@ swirl <- function(
   bridge$x2 <- bridge$x + (curl$x * scale * bridge$len)
   bridge$y2 <- bridge$y + (curl$y * scale * bridge$len)
 
-  background <- "black"
+  return(bridge)
+}
+
+
+#' Style wrap
+#'
+#' @param coil a coiled bridge
+#' @param background colour
+#' @param type line, barbelle or points
+#' @param palette palette generator
+#'
+#' @return ggplot
+#' @export
+style_wrap <- function(
+  coil,
+  file = NULL,
+  background = "black",
+  type = "line",
+  width = 3000, # width in pixels ()
+  height = 3000,
+  palette = palette_viridis()
+) {
+
+  nseries <- max(coil$id)
 
   pic <- ggplot2::ggplot(
-    data = bridge,
+    data = coil,
     mapping = ggplot2::aes(
       x = x, xend = x2,
       y = y, yend = y2,
-      color = series
+      color = factor(id)
     )
   ) +
-#    scico::scale_color_scico() +
+    ggplot2::scale_color_manual(values = palette(nseries, alpha = .6)) +
     theme_mono("black")
 
   if(type == "line" | type == "barbell") {
@@ -44,9 +65,20 @@ swirl <- function(
   if(type == "points" | type == "barbell") {
     pic <- pic +
       ggplot2::geom_point(size = 2, show.legend = FALSE) +
-      ggplot2::geom_point(aes(x = x2, y = y2), show.legend = FALSE)
+      ggplot2::geom_point(ggplot2::aes(x = x2, y = y2), show.legend = FALSE)
   }
 
+  # create the png if requested
+  if(!is.null(file)) {
+    grDevices::png(
+      filename = file,
+      width = width,
+      height = height,
+      bg = background
+    )
+    plot(pic)
+    grDevices::dev.off()
+  }
   return(pic)
 }
 
