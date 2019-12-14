24d18b97da7da2bfa5f0a1470fea6febaf84cdfb
Danielle Navarro <d.navarro@unsw.edu.au>
2019-12-14 14:48:08 +1100

    add ropensci palette and delaunay seed

---
+++ R/seed_delaunay.R
@@ -0,0 +1,42 @@
+#' Generate seed points with a Delaunay triangulation
+#'
+#' @param n Number of vertices
+#' @param grain Number of points along each line
+#'
+#' @return A tibble with columns x, y and id
+#' @export
+seed_delaunay <- function(n = 20, grain = 50) {
+
+	# create the delaunay tiling
+	points <- spatstat::runifpoint(n)
+	del <- spatstat::delaunay(points)
+	del <- as.data.frame(del)
+	names(del) <- c("x", "y", "id")
+	del$id <- gsub("Tile ", "", del$id)
+	del$id <- as.numeric(del$id)
+
+	# define an interpolation function
+	interp <- function(a, b) {
+
+		# don't interpolate lines in different tiles
+		if(a$id != b$id) {
+			return(tibble::tibble(x = numeric(0), y = numeric(0), id = numeric(0)))
+		}
+
+		# otherwise...
+		tibble::tibble(
+			x = seq(a$x, b$x, length.out = grain),
+			y = seq(a$y, b$y, length.out = grain),
+			id = rep(a$id, grain)
+		)
+	}
+
+	del <- purrr::transpose(del)
+	del <- purrr::map2_dfr(del[-1], del[-length(del)], interp)
+
+	return(del)
+
+}
+
+
+
