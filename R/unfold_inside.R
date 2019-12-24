eae7a252fbede73b779df59176b68f24c721dbfe
Danielle Navarro <d.navarro@unsw.edu.au>
2019-12-24 20:35:16 +1100

    adds initial version of unfold_inside()

---
+++ R/unfold_inside.R
@@ -0,0 +1,19 @@
+
+#' Determine whether each point lies inside its seed
+#'
+#' @param data Data
+#' @param output String specifying a Column name
+#'
+#' @return Returns the original data tibble with a new column added
+#' @export
+unfold_inside <- function(data, output = "inside") {
+
+  is_inside <- function(x, y, time) {
+    sp::point.in.polygon(x, y, x[time==1], y[time==1])
+  }
+
+  data %>%
+    dplyr::group_by(id) %>%
+    dplyr::mutate(!!output := is_inside(x, y, time)) %>%
+    dplyr::ungroup()
+}
--- R/zzz.R
+++ R/zzz.R
@@ -8,6 +8,7 @@ utils::globalVariables(c(".", "V1", "V2", "V3", "V4", "V5", "V6", "aes",
                          "char_ind", "id", "series", "time", "x2", "xpos", "y2", "ypos"))
 utils::globalVariables(c("char", "make_bridges", "seed_x", "seed_y"))
 utils::globalVariables(c("xend", "yend", "al"))
+utils::globalVariables(":=")
 
 #
 # img_rescale <- function(from, to, scale) {
