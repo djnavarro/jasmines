a0d167b233f375429e0aeea994cbbfca2ea8ab4b
Danielle Navarro <d.navarro@unsw.edu.au>
2019-12-29 09:22:07 +1100

    add initial version of unfold_worley()

---
+++ R/unfold_worley.R
@@ -0,0 +1,12 @@
+#' Colour points using Worley noise
+#'
+#' @param data the data object
+#' @param seed seed to pass to ambient::gen_worley()
+#' @param output name of the primary unfolding variable to add (e.g., order)
+#' @param ... arguments to pass to ambient::genworley()
+#'
+#' @export
+unfold_worley <- function(data, seed, output = "order", ...) {
+  data[[output]] <- ambient::gen_worley(data$x, data$y, seed = seed, ...)
+  return(data)
+}
