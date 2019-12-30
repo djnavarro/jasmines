0a77a9d721c664bdd2279cf8882825a282859faa
Danielle Navarro <d.navarro@unsw.edu.au>
2019-12-31 08:05:39 +1100

    adds xlim and ylim arguments to export_image()

--- R/save.R
+++ R/save.R
@@ -5,6 +5,8 @@
 #' @param width defaults to 3000 pixels (10in at 300dpi)
 #' @param height defaults to 3000 pixels (10in at 300dpi)
 #' @param dpi defaults to 300dpi
+#' @param xlim by default plot limits are c(-.05, 1.05), relative to data spanning c(0, 1), but can override
+#' @param ylim by default plot limits are c(-.05, 1.05), relative to data spanning c(0, 1), but can override
 #'
 #' @export
 export_image <- function(
@@ -12,8 +14,17 @@ export_image <- function(
   filename,
   width = 10,
   height = 10,
-  dpi = 300
+  dpi = 300,
+  xlim = NULL,
+  ylim = NULL
 ) {
+
+  if(!is.null(xlim) | !is.null(ylim)) {
+    if(is.null(xlim)) xlim <- c(-.05, 1.05)
+    if(is.null(ylim)) ylim <- c(-.05, 1.05)
+    input <- input +  ggplot2::coord_cartesian(xlim = xlim, ylim = ylim)
+  }
+
   ggplot2::ggsave(
     filename = filename, plot = input,
     width = width, height = height,
